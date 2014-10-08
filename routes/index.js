2438f7ee342f83f50e93c81482ec9d890b0fbc05
95civicdude <jeff.lamoureux@bazaarvoice.com>
2014-10-08 13:55:12 -0500

    Twitter API Connect Test

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,18 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  	res.render('index', { title: 'Express' });
+
+  	var util = require('util'),
+    	twitter = require('twitter');
+	var twit = new twitter({
+	    consumer_key: 'iOuQfKzgo3wuAZ7PPCvf12gtn',
+	    consumer_secret: 'Ci6xlaeFi8kOfbmr92DYLh7851lWrOUsfaUyrJ7YzWkVgYio25',
+	    access_token_key: '2847594302-Z0AkMYFeV37RFvvGjQ1OCXoWoqgIMpKS1bFzjP8',
+	    access_token_secret: 'aIXo1fyhuXIIcha1NnGritbFyEL7JDH74mei7PcV5YRua'
+	});
+	
+	twit.search('#liveBV', function(data) {
+    console.log(util.inspect(data));
+	});
 };
