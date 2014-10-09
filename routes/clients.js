c94f78a678913ce727fda93ee66c94408ea1984b
Jeremy A. Love <jeremy.love@bazaarvoice.com>
2014-10-08 20:07:40 -0500

    added client create utility

--- routes/clients.js
+++ routes/clients.js
@@ -1,3 +1,20 @@
+/*
+ * client record schema:
+   {
+        "name" : "jeffs-testcompany",
+        "apiKey" : "xxxxxxxx"
+        "twitterHandle" : "tshacktoberfest",
+        "consumerKey": "iOuQfKzgo3wuAZ7PPCvf12gtn",
+        "consumerSecret": "Ci6xlaeFi8kOfbmr92DYLh7851lWrOUsfaUyrJ7YzWkVgYio25",
+        "accessTokenKey": "2847594302-Z0AkMYFeV37RFvvGjQ1OCXoWoqgIMpKS1bFzjP8",
+        "accessTokenSecret": "aIXo1fyhuXIIcha1NnGritbFyEL7JDH74mei7PcV5YRua",
+        "products" : [
+            "externalId" : "566",
+            "hashTag" : "vue"
+        ]
+    }
+ */
+
 exports.create = function(req, res) {
     var MongoClient = require("mongodb").MongoClient;
 
