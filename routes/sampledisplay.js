6d72d1c94e427604fa44729978d00dc2c0b91073
Mitch Heard <mitchheard@gmail.com>
2014-11-13 14:07:30 -0600

    using apikey now for sampledisplay

--- routes/sampledisplay.js
+++ routes/sampledisplay.js
@@ -1,19 +1,17 @@
 var dbConnection = require("../data/dbConnection.js");
 
 exports.display = function(req, res) {
-	console.log("here1");
 	getClientApiKey(
 		req.param("clientName"), 
 		function(apiKey){
-			console.log("testtt");
-			console.log("test" + apiKey);
-			res.render("sampledisplay", {clientName: req.param("clientName"), hashTag: req.param("hashTag"), apiKey: apiKey});
+			if(!apiKey)
+				res.render("sampledisplay", {error: "not_provisioned"});
+			else res.render("sampledisplay", {clientName: req.param("clientName"), hashTag: req.param("hashTag"), apiKey: apiKey});
 		});
 };
 
 
 var getClientApiKey = function(clientName, callback) {
-	console.log("here2");
     dbConnection.getClientsCollection(function(clients) {
         clients.find({
             "name" : clientName
@@ -25,6 +23,7 @@ var getClientApiKey = function(clientName, callback) {
             if (docs && docs.length) {
                 callback(docs[0].apiKey);
             }
+            else {callback(null);}
         });
     });
 };
