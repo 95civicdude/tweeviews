86c12bf9c0594df32bf6175dfdb55b02d01bd075
Mitch Heard <mitchheard@gmail.com>
2014-11-13 13:30:32 -0600

    sampledisplay stuff

--- routes/sampledisplay.js
+++ routes/sampledisplay.js
@@ -1,3 +1,30 @@
+var dbConnection = require("../data/dbConnection.js");
+
 exports.display = function(req, res) {
-    res.render("sampledisplay", {clientName: req.param("clientName"), hashTag: req.param("hashTag")});
+	console.log("here1");
+	getClientApiKey(
+		req.param("clientName"), 
+		function(apiKey){
+			console.log("testtt");
+			console.log("test" + apiKey);
+			res.render("sampledisplay", {clientName: req.param("clientName"), hashTag: req.param("hashTag"), apiKey: apiKey});
+		});
+};
+
+
+var getClientApiKey = function(clientName, callback) {
+	console.log("here2");
+    dbConnection.getClientsCollection(function(clients) {
+        clients.find({
+            "name" : clientName
+        }).toArray(function(err, docs) {
+            if (err) {
+                throw err;
+            }
+
+            if (docs && docs.length) {
+                callback(docs[0].apiKey);
+            }
+        });
+    });
 };
