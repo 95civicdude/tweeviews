4a8fc37d822ecb9225af07fc07e0c373dc932bb8
Mitch Heard <mitchheard@gmail.com>
2014-11-14 09:29:07 -0600

    more bv bootstrap stuff

---
+++ routes/demo.js
@@ -0,0 +1,62 @@
+var dbConnection = require("../data/dbConnection.js");
+
+exports.display = function(req, res) {
+	getClientApiKey(
+		req.param("clientName"), 
+		function(apiKey){
+			if(!apiKey){
+				res.render("demo", {error: "not_provisioned"});
+            }
+			else{
+                res.render("demo", {clientName: req.param("clientName"), productId: req.param("productId"), apiKey: apiKey});
+            }
+		});
+    // getClientList(
+    //     function(clientList) {
+    //         console.log(clientList);
+    //         if(!apiKey){
+    //             res.render("demo", {error2: "no_clients"});
+    //         }
+    //         else{
+    //             res.render("demo", {clientList: clientList});
+    //         }
+    //     }
+    // )
+};
+
+
+var getClientApiKey = function(clientName, callback) {
+    dbConnection.getClientsCollection(function(clients) {
+        clients.find({
+            "name" : clientName
+        }).toArray(function(err, docs) {
+            if (err) {
+                throw err;
+            }
+
+            if (docs && docs.length) {
+                callback(docs[0].apiKey);
+            }
+            else {callback(null);}
+        });
+    });
+};
+
+var getClientList = function(callback) {
+    // dbConnection.getClientsCollection(function(clients) {
+    //     // TODO: place all client names in an array to callback
+    //     clients.find({
+    //         "name" : clientName
+    //     }).toArray(function(err, docs) {
+    //         if (err) {
+    //             throw err;
+    //         }
+
+    //         if (docs && docs.length) {
+    //             callback(docs);
+    //         }
+    //         else {callback(null);} 
+    //     });
+    // });
+    
+};
---
+++ routes/releasenotes.js
@@ -0,0 +1,3 @@
+exports.display = function(req, res) {
+    res.render("releasenotes");
+};
